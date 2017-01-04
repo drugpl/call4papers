b8ee029cb15680e4d10370a2bfa8ab064e758df6
Rafał Łasocha <me@swistak35.com>
2017-01-04 21:16:19 +0100

    Add secret token for development mode

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,5 @@
-Cfp::Application.config.secret_token = ENV['SECRET_TOKEN']
+if Rails.env.development?
+  Cfp::Application.config.secret_token = "0df6016f9cee2dc2295dd66ef005452f01fb932b8059c3abf3b7adfbf0d11b7b"
+else
+  Cfp::Application.config.secret_token = ENV['SECRET_TOKEN']
+end
