6ccac22e85c9a448369a277fd433a8183121b5f0
Michal Matyas <github@higher.lv>
2020-02-11 11:01:36 +0100

    Allow name to be set when signing up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   skip_before_action :authenticate
+  before_action :configure_permitted_parameters
   layout 'welcome'
 
   def create
@@ -14,5 +15,9 @@ class RegistrationsController < Devise::RegistrationsController
       @user.apply_omniauth(session[:omniauth])
       @user.valid?
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
